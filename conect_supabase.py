f5a16cc0fc23aa768c03ffe381a99a64ed14c0f1
LucSousa21 <souza.lucas1805@gmail.com>
2026-06-18 00:46:38 -0300

    Implementar integração com Supabase para buscar contatos

--- conect_supabase.py
+++ conect_supabase.py
@@ -1,17 +1,21 @@
 import os
-from dotenv import load_dotenv
 from supabase import create_client
 
+# Função para obter dados do Supabase
 def get_supabase_client():
-        
-    load_dotenv()
-    url = os.getenv("SUPABASE_URL")
-    key = os.getenv("SUPABASE_ANON_KEY")
 
+    try:
+        # Carregar variáveis de ambiente    
+        url = os.getenv("SUPABASE_URL")
+        key = os.getenv("SUPABASE_ANON_KEY")
 
-    supabase = create_client(url, key)
+        # Criar cliente do Supabase
+        supabase = create_client(url, key)
+        # Realizar consulta para obter contatos
+        response = supabase.table("contatos").select("*").limit(3).execute()
 
-    response = supabase.table("contatos").select("*").limit(3).execute()
-
+        return response.data
     
-    return response.data
+    except Exception as e:
+        print("Erro ao conectar ao Supabase:", str(e))
+        return []
