d01af58be8c5bca49922c8c211fc7fdf159638c8
LucSousa21 <souza.lucas1805@gmail.com>
2026-06-18 00:48:10 -0300

    Implementar envio de mensagens via Z-API

--- zapi_mensagem.py
+++ zapi_mensagem.py
@@ -1,23 +1,44 @@
-import os, requests
-from urllib import response
+import os
+import requests
 from conect_supabase import get_supabase_client
 
+# Função para enviar mensagem usando a API do ZAPI
 def enviar_mensagem(nome, telefone):
-    url = os.getenv("ZAPI_INSTANCE_URL")
-    token = os.getenv("ZAPI_TOKEN")
+    try:
 
-    payload = {
-        "phone": telefone,
-        "message": f"Olá {nome}, tudo bem?"
-    }
+        # Carregar variáveis de ambiente
+        instance_id = os.getenv("ZAPI_INSTANCE_ID")
+        token = os.getenv("ZAPI_TOKEN")
 
-    response = requests.post(url, json=payload, headers={"Authorization": f"Bearer {token}"})
-    print(response.status_code)
-    print(response.text)
-    if response.status_code != 200:
-        print("Error: ", response.status_code)
+        # Construir a URL da API
+        url = f"https://api.z-api.io/instances/{instance_id}/token/{token}/send-text"
+        
+        # Mensagem personalizada
+        payload = {
+            "phone": telefone,
+            "message": f"Olá {nome}, tudo bem?"
+        }
 
-if __name__ == "__main__":
-    dado = get_supabase_client()
-    enviar_mensagem(dado[1]['nome'], dado[1]['telefone'])
+        # Enviar a requisição POST para a API do ZAPI
+        response = requests.post(url, json=payload)
+
+        # Verificar a resposta da API
+        if response.status_code != 200:
+            print("Error:", response.status_code, response.text)
+            
+        
+        
+        return response.status_code, response.text
+        
+    except requests.RequestException as e:
+        print("Erro de conexão com a API do ZAPI:", str(e))
+        return None, str(e)
+
+
+
+def enviar_mensagens_em_massa():
+    contatos = get_supabase_client()
+    for contato in contatos:
+        enviar_mensagem(contato['nome'], contato['telefone'])
+        
     
